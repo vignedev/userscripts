83d19c424d78454e86d689a3ecc8892d46a8aeb8
vignedev <vignedev@gmail.com>
2019-04-26 21:31:54 +0200

    floating-image.user.js - add video floating video

--- floating-image.user.js
+++ floating-image.user.js
@@ -10,35 +10,52 @@
 
 (function() {
     'use strict';
+    const images = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', ':large', ':orig', ':small', ':medium', ':tiny', '.webp']
+    const videos = ['.webm', '.mp4']
 
     var items = document.querySelectorAll("a")
     var style = document.createElement('style')
     var preview = document.createElement('div')
+    var previewVideo = document.createElement('video')
     var offset = [4,4]
+
     preview.id = 'preview'
     preview.style.display = 'none'
+    previewVideo.id = 'preview'
+    previewVideo.style.display = 'none'
+
     style.innerText = '#preview{position:fixed; width: 320px; height: 320px; box-shadow: 0 0 1em rgba(0,0,0,0.3); background-color: black; background-size: contain; background-repeat: no-repeat; background-position:center center;}'
     document.head.appendChild(style)
     document.body.appendChild(preview)
+    document.body.appendChild(previewVideo)
 
     items.forEach(link => {
         link.onmouseover = update
         link.onmousemove = update
         function update(e) {
-            if(allExtensions(link.href)){
+            if(allExtensions(link.href, images)){
+                previewVideo.style.display = 'none'
                 preview.style.display = ''
                 preview.style.backgroundImage = `url(${link.href})`
                 preview.style.left = `${e.clientX+offset[0]}px`
                 preview.style.top = `${e.clientY+offset[1]}px`
+            }else if(allExtensions(link.href, videos)){
+                preview.style.display = 'none'
+                if(previewVideo.src != link.href) previewVideo.src = link.href
+                previewVideo.play()
+                previewVideo.style.display = ''
+                previewVideo.style.left = `${e.clientX+offset[0]}px`
+                previewVideo.style.top = `${e.clientY+offset[1]}px`
             }
         }
         link.onmouseleave = () => {
             preview.style.display = 'none'
+            previewVideo.style.display = 'none'
+            previewVideo.pause()
         }
     })
 
-    function allExtensions(input){
-        var ext = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', ':large', ':orig', ':small', ':medium', ':tiny', '.webp']
+    function allExtensions(input, ext){
         var valid = false
         input = decodeURIComponent(input.toLowerCase())
         for(var i = 0; i < ext.length; i++){
